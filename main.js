c8a407fb8b17539def123085993ada75b0842736
Ava Smith <ava013005@gmail.com>
2023-07-05 16:36:23 -0500

    done

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    let html = '<div class="coffee">';
+    let html = '<div class="coffee-card">';
     // html += '<td>' + coffee.id + '</td>';
-    html += '<h3>' + coffee.name + '</h3>';
-    html += '<p>' + coffee.roast + '</p>';
+    if(coffee.roast === 'light'){
+        html += '<img src="img/light-roast.jpeg" alt="light roast">'
+    } else if(coffee.roast === 'medium'){
+        html += '<img src="img/medium-roast.jpeg" alt="medium roast">'
+    }else if(coffee.roast === 'dark') {
+        html += '<img src="img/dark-roast.jpeg" alt="dark roast">'
+    }
+    html += '<h3 class="coffee-name">' + coffee.name + '</h3>';
+    html += '<p class="coffee-roast">' + coffee.roast + '</p>';
     html += '</div>';
     return html;
 }
@@ -24,6 +31,8 @@ function updateCoffees(e) {
     coffees.forEach(function (coffee) {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
+        } else if(selectedRoast === 'all'){
+            filteredCoffees.push(coffee);
         }
     });
     mainContent.innerHTML = renderCoffees(filteredCoffees);
@@ -32,7 +41,6 @@ function updateCoffees(e) {
 function searchCoffeeByName(e) {
     e.preventDefault();
     let searchedCoffee = searchBox.value.toLowerCase();
-
     let filteredCoffeeName = [];
     coffees.forEach(function (coffee) {
         if (coffee.name.toLowerCase().includes(searchedCoffee)) {
@@ -42,38 +50,49 @@ function searchCoffeeByName(e) {
     mainContent.innerHTML = renderCoffees(filteredCoffeeName);
 }
 
+function addCoffee (e) {
+    e.preventDefault();
+    let newCoffee = {id: (coffees.length + 1), name: addCoffeeName.value, roast: addCoffeeRoast.value};
+    coffees.push(newCoffee);
+    mainContent.innerHTML = renderCoffees(coffees);
+}
+
+
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 let coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 1, name: 'Pumpkin Spice Latte', roast: 'light'},
+    {id: 2, name: 'Monster Mash Macchiato', roast: 'light'},
+    {id: 3, name: 'London Fog Latte', roast: 'light'},
+    {id: 4, name: 'Spider Eye Latte', roast: 'medium'},
+    {id: 5, name: 'Beetlejuice Macchiato', roast: 'medium'},
+    {id: 6, name: 'Boogie Man Cappuccino', roast: 'medium'},
+    {id: 7, name: 'Witches Brew Latte', roast: 'dark'},
+    {id: 8, name: 'Frankenstein Cappuccino', roast: 'dark'},
+    {id: 9, name: 'Morticia Macchiato', roast: 'dark'},
+    {id: 10, name: 'Poltergeist Cappuccino', roast: 'dark'},
+    {id: 11, name: 'Lycanthropy Latte', roast: 'dark'},
+    {id: 12, name: 'Moon Macchiato', roast: 'dark'},
+    {id: 13, name: 'Hocus Pocus Cappuccino', roast: 'dark'},
+    {id: 14, name: 'Bones Latte', roast: 'dark'},
 ];
 
 let mainContent = document.querySelector('#coffees');
 let submitButton = document.querySelector('#submit');
 let roastSelection = document.querySelector('#roast-selection');
 let searchBox = document.querySelector('#search-box');
+let addCoffeeRoast = document.querySelector('#add-coffee-roast');
+let addCoffeeName = document.querySelector('#add-coffee-name');
+let addButton = document.querySelector('#add-btn');
 
 mainContent.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
 document.addEventListener('keydown', (event) => {
     let keyCode = event.code;
-    console.log(keyCode);
     if(keyCode === 'Enter'){
         updateCoffees();
     }
 });
 searchBox.addEventListener('input', searchCoffeeByName);
+addButton.addEventListener('click', addCoffee);
 
